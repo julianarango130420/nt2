b5c8d37a801beb9b9d909c4f76842fb8fd449d19
Thomas <thomasmadrigal30@gmail.com>
2023-08-11 11:12:25 -0500

    nt2 se agrego una nueva varible

--- codigo1.py
+++ codigo1.py
@@ -1,3 +1,4 @@
 nombreUsuario = input("Ingresar el nombre")
 direccionCasa = input("Ingrsar la dirección")
 edad=(int(input("ingresar edad")))
+cantidadHijos=(int(input("ingresa cantidad de hijos")))
